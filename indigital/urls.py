83fb17e250b44a1d3ce3aca91c6c5875aa9dd21e
Adaylla <a.adaylla@escolar.ifrn.edu.br>
2025-01-26 16:17:52 -0300

    urls, views e templates

--- indigital/urls.py
+++ indigital/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('reserva', views.reserva, name='reserva'),
     path('login', views.login, name='login'),
+    path('esqueceuasenha', views.esqueceuasenha, name='esqueceuasenha'),
+    path('cadastro', views.cadastro, name='cadastro'),
+    path('perfil', views.perfil, name='perfil'),
 ]
